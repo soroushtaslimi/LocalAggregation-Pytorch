10305ccd8052bca080511fa42be5dbbcf0f5944f
Soroush <soroushtaslimi@yahoo.com>
2020-12-24 02:35:09 +0330

    torchvision.datasets root dir bug removed dataset download bug removed

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -938,9 +938,9 @@ class MNISTAgent(BaseAgent):
     def _load_datasets(self):
         train_transforms, test_transforms = self._load_image_transforms()
         # build training dataset
-        train_dataset = datasets.MNIST(train=True, transform=train_transforms)
+        train_dataset = datasets.MNIST(root="/data/MNIST/", train=True, transform=train_transforms, download=True)
         # build validation set
-        val_dataset = datasets.MNIST(train=False, transform=test_transforms)
+        val_dataset = datasets.MNIST(root="/data/MNIST/", train=False, transform=test_transforms, download=True)
 
         # save some stuff to config
         self.config.data_params.n_channels = 3
