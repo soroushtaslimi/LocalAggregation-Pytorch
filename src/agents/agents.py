6ee7608fdff778a9b0487083c0ddb4f58c4aa2a9
Soroush <soroushtaslimi@yahoo.com>
2020-12-24 01:48:27 +0330

    added MNIST support

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -15,7 +15,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torchvision import transforms, models
+from torchvision import transforms, models, dataset
 import torchvision.transforms.functional as tf
 import torch.backends.cudnn as cudnn
 
@@ -833,3 +833,347 @@ class ImageNetFineTuneAgent(BaseAgent):
             )
 
 
+class MNISTAgent(BaseAgent):
+    def __init__(self, config):
+        super(MNISTAgent, self).__init__(config)
+
+        self._init_memory_bank()
+        self._init_cluster_labels()
+
+        loss_class = globals()[self.config.loss_params.loss]  # toggle between ID and LA
+        self._init_loss_function(loss_class, self.memory_bank, self.cluster_labels)
+
+        self.km = None  # will be populated by a kmeans model
+        self.parallel_helper_idxs = torch.arange(len(self.gpu_devices)).to(self.device)
+
+        # if user did not specify kmeans_freq, then set to constant
+        if self.config.loss_params.kmeans_freq is None:
+            self.config.loss_params.kmeans_freq = (
+                    len(self.train_dataset) //
+                    self.config.optim_params.batch_size)
+
+        self.val_acc = []
+        self.train_loss = []
+        self.train_extra = []
+
+        cudnn.benchmark = True
+
+        self.first_iteration_kmeans = True
+
+    def _init_memory_bank(self, attr_name='memory_bank'):
+        data_len = len(self.train_dataset)
+        memory_bank = MemoryBank(data_len, self.config.model_params.out_dim, self.gpu_devices)
+        setattr(self, attr_name, memory_bank)
+
+    def load_memory_bank(self, memory_bank):
+        self._load_memory_bank(memory_bank, attr_name='memory_bank')
+
+    def _load_memory_bank(self, memory_bank, attr_name='memory_bank'):
+        _bank = memory_bank.bank_broadcast[0].cpu()
+        _bank_broadcast = torch.cuda.comm.broadcast(_bank, self.gpu_devices)
+        self._get_memory_bank(attr_name).bank_broadcast = _bank_broadcast
+        self._get_loss_func('loss_fn').module.memory_bank_broadcast = self._get_memory_bank(attr_name).bank_broadcast
+
+    def get_memory_bank(self):
+        return self._get_memory_bank(attr_name='memory_bank')
+
+    def _get_memory_bank(self, attr_name='memory_bank'):
+        return getattr(self, attr_name)
+
+    def _get_loss_func(self, attr_name='loss_fn'):
+        return getattr(self, attr_name)
+
+    def _init_cluster_labels(self, attr_name='cluster_labels'):
+        no_kmeans_k = self.config.loss_params.n_kmeans  # how many wil be train
+        data_len = len(self.train_dataset)
+        # initialize cluster labels
+        cluster_labels = torch.arange(data_len).long()
+        cluster_labels = cluster_labels.unsqueeze(0).repeat(no_kmeans_k, 1)
+        broadcast_cluster_labels = torch.cuda.comm.broadcast(cluster_labels, self.gpu_devices)
+        setattr(self, attr_name, broadcast_cluster_labels)
+
+    def _init_loss_function(self, loss_class, memory_bank, cluster_label=None, attr_name='loss_fn'):
+        loss_fn = loss_class(memory_bank.bank_broadcast,
+                             cluster_label,
+                             k=self.config.loss_params.k,
+                             t=self.config.loss_params.t,
+                             m=self.config.loss_params.m)
+        loss_fn = nn.DataParallel(loss_fn, device_ids=self.gpu_devices).to(self.device)
+        setattr(self, attr_name, loss_fn)
+
+    def _load_image_transforms(self):
+
+        image_size = self.config.data_params.image_size
+        if self.config.data_params.image_augment:
+            train_transforms = transforms.Compose([
+                transforms.RandomResizedCrop(image_size, scale=(0.2, 1.)),
+                transforms.RandomGrayscale(p=0.2),
+                transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]),
+            ])
+
+            test_transforms = transforms.Compose([
+                transforms.Resize(256),  # FIXME: hardcoded for 224 image size
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]),
+            ])
+
+        else:
+            train_transforms = transforms.Compose([
+                transforms.Resize(256),  # FIXME: hardcoded for 224 image size
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225]),
+            ])
+            test_transforms = copy.copy(train_transforms)
+
+        return train_transforms, test_transforms
+
+    def _load_datasets(self):
+        train_transforms, test_transforms = self._load_image_transforms()
+        # build training dataset
+        train_dataset = dataset.MNIST(train=True, transform=train_transforms)
+        # build validation set
+        val_dataset = dataset.MNIST(train=False, transform=test_transforms)
+
+        # save some stuff to config
+        self.config.data_params.n_channels = 3
+
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
+
+        train_samples = train_dataset.dataset.samples
+        train_labels = [train_samples[i][1] for i in range(len(train_samples))]
+        self.train_ordered_labels = np.array(train_labels)
+
+    def _create_model(self):
+        assert self.config.data_params.image_size == 224
+
+        if self.config.model_params.resnet_version.startswith('preact-'):
+            model = PreActResNet18(num_classes=self.config.model_params.out_dim)
+        elif self.config.model_params.resnet_version.startswith('resnet'):
+            resnet_class = getattr(models, self.config.model_params.resnet_version)
+            model = resnet_class(pretrained=False,
+                                 num_classes=self.config.model_params.out_dim)
+        else:
+            raise NotImplementedError
+
+        model = nn.DataParallel(model)
+        model = model.to(self.device)
+        cudnn.benchmark = True
+
+        self.model = model
+
+    def _set_models_to_eval(self):
+        self.model = self.model.eval()
+
+    def _set_models_to_train(self):
+        self.model = self.model.train()
+
+    def _create_optimizer(self):
+        # Exclude batch norm weights and bias from weight decay
+        parameters = exclude_bn_weight_bias_from_weight_decay(self.model,
+                                                              weight_decay=self.config.optim_params.weight_decay)
+
+        self.optim = torch.optim.SGD(parameters,
+                                     lr=self.config.optim_params.learning_rate,
+                                     momentum=self.config.optim_params.momentum,
+                                     weight_decay=self.config.optim_params.weight_decay)
+
+    def train_one_epoch(self):
+        num_batches = self.train_len // self.config.optim_params.batch_size
+        tqdm_batch = tqdm(total=num_batches,
+                          desc="[Epoch {}, lr {}]".format(self.current_epoch, self.optim.param_groups[0]['lr']))
+
+        self._set_models_to_train()  # turn on train mode
+        epoch_loss = AverageMeter()
+
+        for batch_i, (indices, images, labels) in enumerate(self.train_loader):
+            batch_size = images.size(0)
+
+            # cast elements to CUDA
+            indices = indices.to(self.device, non_blocking=True)
+            images = images.to(self.device, non_blocking=True)
+
+            # do a forward pass
+            outputs = self.model(images)
+
+            # compute the loss
+            loss, new_data_memory = self.loss_fn(indices, outputs, self.parallel_helper_idxs)
+            loss = torch.mean(loss)  # average the loss from multi-gpus to obtain a scalar value
+
+            self.optim.zero_grad()
+            loss.backward()
+            self.optim.step()
+
+            with torch.no_grad():
+                self.memory_bank.update(indices, new_data_memory)
+                self.loss_fn.module.memory_bank_broadcast = self.memory_bank.bank_broadcast  # update loss_fn
+
+                if (self.config.loss_params.loss == 'LocalAggregationLossModule' and
+                        (self.first_iteration_kmeans or batch_i == self.config.loss_params.kmeans_freq)):
+
+                    if self.first_iteration_kmeans:
+                        self.first_iteration_kmeans = False
+
+                    self.logger.info('Fitting K-means with FAISS')
+
+                    # get kmeans clustering (update our saved clustering)
+                    k = [self.config.loss_params.kmeans_k for _ in
+                         range(self.config.loss_params.n_kmeans)]
+
+                    # NOTE: we use a different gpu for FAISS otherwise cannot fit onto memory
+                    self.km = Kmeans(k, self.memory_bank, gpu_device=self.gpu_devices)
+                    cluster_labels = self.km.compute_clusters()
+
+                    # broadcast the cluster_labels after modification
+                    for i in range(len(self.gpu_devices)):
+                        device = self.loss_fn.module.cluster_label_broadcast[i].device
+                        self.loss_fn.module.cluster_label_broadcast[i] = cluster_labels.to(device)
+
+                    # self.logger.info('Memory bank synchronization check')
+                    # self.loss_fn.module.synchronization_check()
+
+            epoch_loss.update(loss.item(), batch_size)
+            tqdm_batch.set_postfix({"Loss": epoch_loss.avg})
+
+            self.summary_writer.add_scalars("epoch/loss", {'loss': epoch_loss.val},
+                                            self.current_iteration)
+
+            self.train_loss.append(epoch_loss.val)
+
+            self.current_iteration += 1
+            tqdm_batch.update()
+
+        self.current_loss = epoch_loss.avg
+        tqdm_batch.close()
+
+    def validate(self):
+        # For validation, for each image, we find the closest neighbor in the
+        # memory bank (training data), take its class! We compute the accuracy.
+
+        num_batches = self.val_len // self.config.optim_params.batch_size
+        tqdm_batch = tqdm(total=num_batches, desc="[Val]")
+
+        self._set_models_to_eval()
+        num_correct = 0.
+        num_total = 0.
+
+        with torch.no_grad():
+            for _, images, labels in self.val_loader:
+                batch_size = images.size(0)
+
+                # cast elements to CUDA
+                images = images.to(self.device)
+                outputs = self.model(images)
+
+                # use memory bank to ge the top 1 neighbor
+                # from the training dataset
+                all_dps = self.memory_bank.get_all_dot_products(outputs)
+                _, neighbor_idxs = torch.topk(all_dps, k=1, sorted=False, dim=1)
+                neighbor_idxs = neighbor_idxs.squeeze(1)  # shape: batch_size
+                neighbor_idxs = neighbor_idxs.cpu().numpy()  # convert to numpy
+                # fetch the actual label of each example
+                neighbor_labels = self.train_ordered_labels[neighbor_idxs]
+                neighbor_labels = torch.from_numpy(neighbor_labels).long()
+
+                num_correct += torch.sum(neighbor_labels == labels).item()
+                num_total += batch_size
+
+                tqdm_batch.set_postfix({"Val Accuracy": num_correct / num_total})
+                tqdm_batch.update()
+
+        self.summary_writer.add_scalars("Val/accuracy",
+                                        {'accuracy': num_correct / num_total},
+                                        self.current_epoch)
+
+        self.current_val_iteration += 1
+        self.current_val_metric = num_correct / num_total
+
+        if self.current_val_metric >= self.best_val_metric:  # NOTE: >= for accuracy
+            self.best_val_metric = self.current_val_metric
+
+        tqdm_batch.close()
+
+        # store the validation metric from every epoch
+        self.val_acc.append(self.current_val_metric)
+
+        return self.current_val_metric
+
+    def load_checkpoint(self, filename, checkpoint_dir=None,
+                        load_memory_bank=True, load_model=True,
+                        load_optim=True, load_epoch=True):
+        checkpoint_dir = checkpoint_dir or self.config.checkpoint_dir
+        filename = os.path.join(checkpoint_dir, filename)
+        try:
+            self.logger.info("Loading checkpoint '{}'".format(filename))
+            checkpoint = torch.load(filename, map_location='cpu')
+
+            if load_epoch:
+                self.current_epoch = checkpoint['epoch']
+                self.current_iteration = checkpoint['iteration']
+                self.current_val_iteration = checkpoint['val_iteration']
+
+            if load_model:
+                model_state_dict = checkpoint['model_state_dict']
+                self.model.load_state_dict(model_state_dict)
+
+            if load_optim:
+                optim_state_dict = checkpoint['optim_state_dict']
+                self.optim.load_state_dict(optim_state_dict)
+
+                lr_pretrained = self.optim.param_groups[0]['lr']
+                lr_config = self.config.optim_params.learning_rate
+
+                # Change learning rate
+                if not lr_pretrained == lr_config:
+                    for param_group in self.optim.param_groups:
+                        param_group['lr'] = self.config.optim_params.learning_rate
+
+            if load_memory_bank:  # load memory_bank
+                self._load_memory_bank(checkpoint['memory_bank'])
+
+            self.logger.info("Checkpoint loaded successfully from '{}' at (epoch {}) at (iteration {}) "
+                             "with val acc = {}\n"
+                             .format(filename, checkpoint['epoch'], checkpoint['iteration'], checkpoint['val_acc']))
+        except OSError as e:
+            self.logger.info("Checkpoint doesnt exists: [{}]".format(filename))
+            raise e
+
+    def copy_checkpoint(self, filename="checkpoint.pth.tar"):
+        if self.current_epoch % self.config.copy_checkpoint_freq == 0:
+            copy_snapshot(
+                filename=filename, folder=self.config.checkpoint_dir,
+                copyname='checkpoint_epoch{}.pth.tar'.format(self.current_epoch),
+            )
+
+    def save_checkpoint(self, filename="checkpoint.pth.tar"):
+        out_dict = {
+            'model_state_dict': self.model.state_dict(),
+            'optim_state_dict': self.optim.state_dict(),
+            'memory_bank': self.memory_bank,
+            'cluster_labels': self.cluster_labels,
+            'epoch': self.current_epoch,
+            'iteration': self.current_iteration,
+            'loss': self.current_loss,
+            'val_iteration': self.current_val_iteration,
+            'val_metric': self.current_val_metric,
+            'config': self.config,
+            'val_acc': np.array(self.val_acc),
+            'train_loss': np.array(self.train_loss),
+            'train_extra': np.array(self.train_extra),
+        }
+
+        # if we aren't validating, then every time we save is the
+        # best new epoch!
+        is_best = ((self.current_val_metric == self.best_val_metric) or
+                   not self.config.validate)
+        save_snapshot(out_dict, is_best, filename=filename,
+                      folder=self.config.checkpoint_dir)
+        # self.copy_checkpoint()
